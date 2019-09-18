f5dcaa6d255c2084ffcf7a7720c229aee3c6392f
laurensjan1999 <54667374+laurensjan1999@users.noreply.github.com>
2019-09-18 13:14:25 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log ("het werkt")
+
+
+const getal = 9 ;
+
+ if (getal%2 == 0 ) {console.log  ("Je hebt een even getal ") ; }
+ else (console.log ("je hebt een oneven getal"))
+//is het getal even of oneven
+
+ const sentence = "programming is not cool"
+ const sentenceReplace = sentence.replace (" not" , "")
+ console.log (sentenceReplace)
+//verwijder het woord not
+
+ const waarde = 1400 ;
+ const zin = "Ik woon in Naboo" ; 
+
+if (waarde == zin) {console.log ("Ze zijn hetzelfde");}
+if (waarde > zin) {console.log ("waarde is groter dan zin");}
+if (waarde < zin) { console.log ("waarde is kleiner dan zin")}
+else console.log ("deze twee waarde zijn niet te vergellijken")
+// Deze vergelijking is waardeloos want je kunt een tekst ofwel string niet vergelijken met een nummer.
